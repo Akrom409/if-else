98312eee5cdc8331cf733b60eaa5f72bb5b227bf
Akrom Abdulvokhidov <68766856+Akrom409@users.noreply.github.com>
2021-01-10 16:54:09 +0500

    Update Shopping.js

--- Shopping.js
+++ Shopping.js
@@ -1,1 +1,13 @@
+// This is a boolean or in otherword logical operator to buy or not to buy thing 
+// Bu mantiqiy amal bajaruvchi kichik dastur unda narsa sotib olish yoki olmaslik talab etiladi. 
+/*Akrom Abdulvokhidov 2021*/
 
+var pulimiz = 10000;          // Bizning pulimiz va valyuta
+var narxi = 9000;             // Maxsulot narxi va valyuta
+
+if(pulimiz>=narxi) {
+  console.log("Maxsulotni olamiz");
+}
+else {
+  console.log("Maxsulotni olmaymiz"); 
+}
